b0a3203b9d70a55d56faf2be514f0dbd45ef46db
sowmya golla <sowmya.golla@xcubelabs.com>
2021-09-20 15:47:29 +0530

    save

--- src/app/shared/shared/shared.module.ts
+++ src/app/shared/shared/shared.module.ts
@@ -1,16 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../components/navbar/navbar.component';
-<<<<<<< HEAD
-import {MatToolbarModule} from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card'
-import { MatButtonModule } from '@angular/material/button'
-
-
-=======
 import { MaterialModule } from '../MaterialModule/material.module';
->>>>>>> 032005f751da86e2e876d2fecf57dd79e05eadc9
 
 @NgModule({
   declarations: [
@@ -18,26 +9,11 @@ import { MaterialModule } from '../MaterialModule/material.module';
   ],
   imports: [
     CommonModule,
-<<<<<<< HEAD
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    MatCardModule
-    
-=======
     MaterialModule
->>>>>>> 032005f751da86e2e876d2fecf57dd79e05eadc9
   ],
   exports: [
     NavbarComponent,
-<<<<<<< HEAD
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    MatCardModule
-=======
     MaterialModule
->>>>>>> 032005f751da86e2e876d2fecf57dd79e05eadc9
   ]
 })
 export class SharedModule { }
